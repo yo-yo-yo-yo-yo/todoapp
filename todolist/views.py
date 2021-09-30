0dc10666700fbd5e99170d05e238f9ce00b09da4
Hana Fathima <hanafathima3172001@gmail.com>
2021-09-30 20:55:33 +0530

    Added form to capture data and implemented button functions

--- todolist/views.py
+++ todolist/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todolist
+from .forms import TodoListForm
+from django.views.decorators.http import require_POST
 # Create your views here.
 def index(request):
     todo_items=Todolist.objects.order_by('id')
     context={'todo_items':todo_items}
     return render(request, 'todolist/index.html',context)
+@require_POST
+def addTodoItem(request)
+    form=TodoListForm(request.POST)
+    print(request.POST['text'])
+    return redirect('index')
