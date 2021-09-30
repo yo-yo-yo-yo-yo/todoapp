6eacfe2966e1696125ec95b7bc741850d3e8ec1e
Hana Fathima <hanafathima3172001@gmail.com>
2021-09-30 20:56:29 +0530

    Added form form.py

---
+++ todolist/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+class TodoListForm(forms.Form):
+    text=forms.CharField*max_length=45
+       widget=forms.TextInput(
+           attrs={'class':'form-control','placeholder':'Enter todo e.g Grocery Shopping','aria-label':'Todo','aria-describeby':'add-btn'})) 
